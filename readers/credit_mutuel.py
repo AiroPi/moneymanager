ccea9c35a93df243a3ab867d30b6812cad0b1fe1
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2024-12-16 11:19:58 +0100

    feat[readers]: improve credti mutuel instructions

--- readers/credit_mutuel.py
+++ readers/credit_mutuel.py
@@ -1,10 +1,31 @@
 """
 # Export instructions
 
-- Connect to https://www.creditmutuel.fr/
-- Go to https://www.creditmutuel.fr/fr/banque/budget.html#/pfm/transactions
+- Connect to [https://www.creditmutuel.fr/](https://www.creditmutuel.fr/)
+- Go to [https://www.creditmutuel.fr/fr/banque/budget.html#/pfm/transactions](https://www.creditmutuel.fr/fr/banque/budget.html#/pfm/transactions)
 - In the "Paramètres" tab, select all the accounts you want to include (probably all of your accounts)
 - in the "Opérations" tab, click the "download" icon
+
+<!--
+Some efforts have also been made to be able to import transactions from an **account export** (not a **budget export**) without duplication.
+However, you should respect the following instructions:
+- Connect to https://www.creditmutuel.fr/
+- Go to https://www.creditmutuel.fr/fr/banque/compte/telechargement.cgi
+- Select format "CSV"
+- Select the following values:
+    - Format               : `Excel XP et suivants`
+    - Dates                : `Français long (JJ/MM/AAAA)`
+    - Séparateur de champs : `point-virgule`
+    - Montants sur         : `une seule colonne`
+    - Séparateur décimal   : `virgule`
+
+You will have to select your accounts one by one before downloading.
+-->
+
+Unfortunately, you can't import your transactions from an **account export**. You need to activate the **budget** feature, and export from here.
+
+If you encounter any problem, please fill an issue at [https://github.com/AiroPi/moneymanager/issues](https://github.com/AiroPi/moneymanager/issues).
+
 """
 
 from __future__ import annotations
@@ -14,8 +35,6 @@ import io
 from collections.abc import Generator
 from datetime import datetime
 from decimal import Decimal
-
-# from decimal import Decimal
 from typing import TYPE_CHECKING, Never
 
 from moneymanager.reader import ReaderABC, Transaction
@@ -91,7 +110,7 @@ class CreditMutuelReader2(ReaderABC):
 
         if cls.header_match(header):
             raise ValueError(
-                "Detected bad export for Crédit Mutuel : please export your operations at https://www.creditmutuel.fr/fr/banque/budget.html#/pfm/transactions instead."
+                "Detected bad export for Crédit Mutuel : please read the instructions before importing. (`moneymanager reader instructions readers/credit_mutuel.py`)."
             )
         return None
 
