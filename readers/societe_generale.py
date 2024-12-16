f6e0795675a4b9e5dfc9f56ead09baf7fcc53993
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2024-12-16 11:20:27 +0100

    feat[readers]: support more export type for Société Générale

--- readers/societe_generale.py
+++ readers/societe_generale.py
@@ -1,3 +1,14 @@
+"""
+# Export instructions
+
+- Connect to https://particuliers.sg.fr/
+- Go to https://particuliers.sg.fr/icd/gbi/index-gbi-authsec.html#/telecharger-operations
+- Click the "download" button
+
+Some efforts have also been made to be able to import transactions from an **account export** (not a **budget** export) without duplication.
+If you encounter any problem, please fill an issue at https://github.com/AiroPi/moneymanager/issues.
+"""
+
 from __future__ import annotations
 
 import hashlib
@@ -5,8 +16,6 @@ import io
 from collections.abc import Generator
 from datetime import datetime
 from decimal import Decimal
-
-# from decimal import Decimal
 from typing import TYPE_CHECKING
 
 from moneymanager.reader import ReaderABC, Transaction
@@ -16,7 +25,7 @@ if TYPE_CHECKING:
     from moneymanager.reader import CSVReader
 
 
-class SocieteGeneraleReader(ReaderABC):
+class SocieteGeneraleAccountExportReader(ReaderABC):
     def __init__(self, file: io.TextIOBase, account: str):
         super().__init__(file, header_lines=3)
         self.account = account
@@ -31,7 +40,7 @@ class SocieteGeneraleReader(ReaderABC):
             account=self.account,
             id=self.generate_id(row),
             amount=Decimal(row[3].replace(",", ".")),
-            label=row[2],
+            label=self.get_label(row),
             date=datetime.strptime(row[0], r"%d/%m/%Y"),
         )
 
@@ -40,10 +49,9 @@ class SocieteGeneraleReader(ReaderABC):
         timestamp = int(date.timestamp())
 
         hasher = hashlib.md5()  # noqa: S324
-        hasher.update(row[1].encode())
-        hasher.update(row[2].encode())
+        hasher.update(self.account.encode())
+        hasher.update(self.get_label(row).encode())
         hasher.update(row[3].encode())
-        hasher.update(row[4].encode())
         hashed_rows = hasher.hexdigest()
 
         return self.fix_id(hashed_rows, timestamp)
@@ -54,7 +62,13 @@ class SocieteGeneraleReader(ReaderABC):
 
     @staticmethod
     def get_account(first_line: str):
-        return first_line.split('"')[1]
+        return first_line.split('"')[1][5:]
+
+    @staticmethod
+    def get_label(row: list[str]):
+        if row[1].startswith("CARTE"):
+            return " ".join(fix_string(row[2]).split(" ")[:-1])
+        return fix_string(row[2])
 
     @classmethod
     def detect_file_impl(cls, file: io.BufferedReader):
@@ -70,5 +84,54 @@ class SocieteGeneraleReader(ReaderABC):
         return None
 
 
+class SocieteGeneraleBudgetExportReader(ReaderABC):
+    def __init__(self, file: io.TextIOBase):
+        super().__init__(file, header_lines=3)
+
+    def generator(self, reader: CSVReader) -> Generator[Transaction]:
+        for row in reader:
+            yield self.row_parser(row)
+
+    def row_parser(self, row: list[str]) -> Transaction:
+        return Transaction(
+            bank="Société Générale",
+            account=row[2],
+            id=self.generate_id(row),
+            amount=Decimal(row[8].replace(",", ".")),
+            label=fix_string(row[5]),
+            date=datetime.strptime(row[0], r"%d/%m/%Y"),
+        )
+
+    def generate_id(self, row: list[str]):
+        date = datetime.strptime(row[0], r"%d/%m/%Y")
+        timestamp = int(date.timestamp())
+
+        hasher = hashlib.md5()  # noqa: S324
+        hasher.update(row[2].encode())
+        hasher.update(fix_string(row[5]).encode())
+        hasher.update(row[8].encode())
+        hashed_rows = hasher.hexdigest()
+
+        return self.fix_id(hashed_rows, timestamp)
+
+    @staticmethod
+    def header_match(header: str):
+        return (
+            fix_string(header)
+            == "Date transaction;Date comptabilisation;Num Compte;Libellé Compte;Libellé opération;Libellé complet;Catégorie;Sous-Catégorie;Montant;Pointée;"
+        )
+
+    @classmethod
+    def detect_file_impl(cls, file: io.BufferedReader):
+        try:
+            header = file.readline(300).decode(encoding="cp1252")
+        except (UnicodeDecodeError, AttributeError):
+            return None
+
+        if cls.header_match(header):
+            return cls(io.TextIOWrapper(file, encoding="cp1252"))
+        return None
+
+
 def export() -> list[type[ReaderABC]]:
-    return [SocieteGeneraleReader]
+    return [SocieteGeneraleAccountExportReader, SocieteGeneraleBudgetExportReader]
