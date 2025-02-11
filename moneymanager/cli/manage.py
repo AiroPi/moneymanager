e0daf6af0c4621cc67285d9bd24082b9610ec0eb
AiroPi <47398145+AiroPi@users.noreply.github.com>
2025-02-11 11:09:56 +0100

    feat: improve and fix the TUI for group management

--- moneymanager/cli/manage.py
+++ moneymanager/cli/manage.py
@@ -1,5 +1,8 @@
+from os import environ
+
 import typer
 
+from ..cache import cache
 from .cli_utils import with_load
 
 manage_subcommands = typer.Typer(no_args_is_help=True, help="Commands to manage your groups, etc.")
@@ -15,6 +18,9 @@ def manage_groups():
     """
     # Import is here to speedup the autocompletion, because textual takes ~0.1s to load.
     from moneymanager.textual_apps import ManageGroupsApp
+
+    if cache.debug_mode:
+        environ["TEXTUAL"] = ",".join(("debug", "devtools", environ.get("TEXTUAL", "")))
 
     app = ManageGroupsApp()
     app.run()
